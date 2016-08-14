fec07f366d046e840612d008a2c949309adb38d0
Kylie Soderblom <MacDream@Kylies-MacBook-Pro.local>
2016-08-14 16:06:13 -0700

    multiple returns on  button click, no gif switch

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,63 +1,52 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
-    var topics =["Elephant", "Zebra", "Rhinoceros", "Monkey", "Tiger", "Lion"];
-    console.log(topics);
+    console.log( "ready!" )
+    //var topics =["Elephant", "Zebra", "Rhinoceros", "Monkey", "Tiger", "Lion"];
+    //console.log(topics);
     arrbtn();
+       // lines 52 - 60 arrbtn function working
+    function arrbtn(){
+        var topics =["Elephant", "Zebra", "Rhinoceros", "Monkey", "Tiger", "Lion"];
+        for (var i = 0; i < topics.length; i++) {
+            var button = $("<button>").addClass("clickable"); //added class clickable, not sure
+            button.attr("data-animal", topics[i]);  // button.attr("name",topics[i]);
+            button.text(topics[i]); // name of animal on button
+            //button.addClass("clickable");
+            $("#buttonSpace").append(button);
+            console.log (topics [i]);
+
+            }    
+                      
+    }  
+   
      $(document.body).on('click', '.clickable', function(){
     // if $("input"), then as soon as they click, creates blank button, however the if/else statement is not helping and nothing console.logs. .submit() not successful
-    $('#submit').on('click', function(){
-        var animalNew= $('#newAnimal').val();
+    $('#submit').on('click', function(event){
+        var animalNew= $('input').val();
         // if(animalNew!=" "){
         console.log(animalNew);
-        newB = $('<button>').addClass('first').text(animalNew);
-        //newB.attr(data-animal, animalNew); 
-        newB.attr(animalNew);
+        newB = $('<button>').addClass('clickable').text(animalNew);
+        newB.attr("data-state", "still"); 
+        //newB.text('input').val();
+        console.log (animalNew);
         $('#buttonSpace').append(newB);
-        fetchAnimals();
+        //fetchAnimals();
 
         return false;
 
     });
-    // else{
-    //     alert ("Please type your choice of animal, blank spaces cannot be heard.")
-    //     return false;
-    // }
-    // });
-    // //  - NOT working Submit Button - new input 2 checks, 1 if >15 then create & push new button val to array and remove 0-array.length-1
-    // $("input").click(function(){
-    //     var newB= $("#newAnimal").val();
-    //     console.log (newB);
-    //     if(topics.length < 5){
-    // alert("Whoa! Time to start a new safari! Click on 'New Safari' to start again!");
 
-    // topics = [];
-    // $("<button>").addClass('first').text("New Safari");
-    
-    
-    //     topics.push(newB);
-    //     arrbtn();
-        // topics.remove (0, array.length - 1);  add }  and });
-    
-    // lines 52 - 60 arrbtn function working
-    function arrbtn(){
-        for (var i = 0; i < topics.length; i++) {
-            var button = $("<button>"); 
-            button.attr("data-animal", topics[i]);  // button.attr("name",topics[i]);
-            button.text(topics[i]); // name of animal on button
-            button.addClass("clickable");
-            $("#buttonSpace").append(button);
-            console.log (topics [i]);
-            }               
-    }   
-    //  lines 63 - 97 working button click routine
-    
-    function fetchAnimals(animalName) {
 
-    }
+    
+    //function fetchAnimals(animalName) {
+
+       // }
+
+       
+    //}
 
     $('button').on("click", function() {
         var animal = $(this).data("animal");
-        fetchAnimals(animal);
+        //fetchAnimals(animal);
          // pull name of animal from button clicked
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10"; // do search using variable 'animal'
 
@@ -91,10 +80,56 @@ $( document ).ready(function() {
             }; //end of for loop
         }); //end of done function
    });//end of button click
+         // //following is flip still/animate function  Problem 89-101
+        $(".gifClass").on('click', function(event){
+            //var animate = response.data.images.fixed_height.url;
+            //var still =   response.data.images.fixed_height_still.url;
+            var state = $(this).attr('data-state');
+            console.log(state);
+        if(state === 'still'){
+            $(this).attr('src',$(this).data('animate'));
+            $(this).attr('data-state','animate');
+        }
+        else{
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state','still');
+        }
+          }); //end of animalDiv click function
 }); //end of clickable
 
 
-    // Make new button (from input line 19 - id newAnimal) Problem 75-87
+    
+       
+
+    //}); //end of function event
+    //var animate = results.images.fixed_height.url
+    //var still =   results.images.fixed_height_still.url
+
+}); //end of doc ready  
+// at time of input.....
+// else{
+    //     alert ("Please type your choice of animal, blank spaces cannot be heard.")
+    //     return false;
+    // }
+    // });
+    // //  - NOT working Submit Button - new input 2 checks, 1 if >15 then create & push new button val to array and remove 0-array.length-1
+    // $("#submit").click(function(){
+    //     var newB= $("input").val();
+    //     console.log (newB);
+
+    // Once you get submit button click function working - (remember also input click on return)
+    // then try lines 115-124
+    //     if(topics.length < 5){
+    // alert("Whoa! Time to start a new safari! Click on 'New Safari' to start again!");
+
+    // topics = [];
+    // $("<button>").addClass('first').text("New Safari");
+    
+    
+    //     topics.push(newB);
+    //     arrbtn();
+        // topics.remove (0, array.length - 1);  add }  and });
+// Make new button (from input line 19 - id newAnimal) Problem 75-87
         // $("input").click(function(){
         //     //event.preventDefault(); // don't want click submit to refresh page, 
         //     var animal = $("#newAnimal").val();
@@ -112,29 +147,6 @@ $( document ).ready(function() {
         // }
         // });
        
-
-        // //following is flip still/animate function  Problem 89-101
-        $(".gifClass").on('click', function(event){
-            var animate = response.data.images.fixed_height.url;
-            var still =   response.data.images.fixed_height_still.url;
-            var state = $(this).attr('data-state');
-            console.log(state);
-        if(state === 'still'){
-            $(this).attr('src',$(this).data('animate'));
-            $(this).attr('data-state','animate');
-        }
-        else{
-            $(this).attr('src', $(this).data('still'));
-            $(this).attr('data-state','still');
-        }
-          }); //end of animalDiv click function
-
-    //}); //end of function event
-    //var animate = results.images.fixed_height.url
-    //var still =   results.images.fixed_height_still.url
-
-}); //end of doc ready  
-
 
 // //     
 // Example of slicing--- practice -
